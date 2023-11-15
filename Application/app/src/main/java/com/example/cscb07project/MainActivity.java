7186d3957c928c59fdba9028281917e71cc5b8a7
Vincent W <93243454+vincent-vw@users.noreply.github.com>
2023-11-15 12:10:07 -0500

    Completed announcement posting feature

--- Application/app/src/main/java/com/example/cscb07project/MainActivity.java
+++ Application/app/src/main/java/com/example/cscb07project/MainActivity.java
@@ -67,4 +67,5 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
 }
--- Application/app/src/main/java/com/example/cscb07project/ui/createannouncement/CreateAnnouncementFragment.java
+++ Application/app/src/main/java/com/example/cscb07project/ui/createannouncement/CreateAnnouncementFragment.java
@@ -4,17 +4,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.cscb07project.MainActivity;
+import com.example.cscb07project.R;
 import com.example.cscb07project.databinding.FragmentCreateAnnouncementBinding;
 import com.example.cscb07project.ui.createannouncement.CreateAnnouncementViewModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class CreateAnnouncementFragment extends Fragment {
     private FragmentCreateAnnouncementBinding binding;
+    FirebaseDatabase db;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,7 +34,27 @@ public class CreateAnnouncementFragment extends Fragment {
         binding = FragmentCreateAnnouncementBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        root.findViewById(R.id.button_new_announcement).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickPostAnnouncement();
+            }
+        });
+
+        // Link to realtime database
+        db = FirebaseDatabase.getInstance("https://cscb07project-c6a1c-default-rtdb.firebaseio.com/");
+
         return root;
+    }
+
+    public void onClickPostAnnouncement(){
+        DatabaseReference ref = db.getReference(); // Reference to the root node
+        // https://stackoverflow.com/questions/48654071/android-studio-how-can-i-put-this-activity-in-extends-fragment
+        EditText userText = (EditText) getView().findViewById(R.id.editText_new_announcement); // Get the EditText widget (used for entering text)
+        String announcement = userText.getText().toString();
+        userText.setText(""); // Clear userText
+        String key = db.getReference(announcement).push().getKey(); // Get a unique key
+        ref.child("announcements").child(key).setValue(announcement); // Set announcement with the unique key as key, announcement as value
     }
 
     @Override
