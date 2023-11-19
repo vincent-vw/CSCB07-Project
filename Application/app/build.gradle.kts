74174c8a2e1b56a217f5550fc5c4dae12c4cb9a9
Vincent W <93243454+vincent-vw@users.noreply.github.com>
2023-11-19 17:39:33 -0500

    Added view announcements feature

--- Application/app/build.gradle.kts
+++ Application/app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui:2.7.5")
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("com.firebaseui:firebase-ui-database:8.0.2")
+    implementation("com.google.android.material:material:1.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- Application/app/src/main/java/com/example/cscb07project/ui/viewannouncements/ViewAnnouncementsFragment.java
+++ Application/app/src/main/java/com/example/cscb07project/ui/viewannouncements/ViewAnnouncementsFragment.java
@@ -9,17 +9,25 @@ import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.cscb07project.MainActivity;
 import com.example.cscb07project.R;
 import com.example.cscb07project.databinding.FragmentViewAnnouncementsBinding;
 import com.example.cscb07project.ui.viewannouncements.ViewAnnouncementsViewModel;
-import com.example.cscb07project.ui.createannouncement.Announcement;
+import com.example.cscb07project.ui.Announcement;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+// Credit to: https://www.geeksforgeeks.org/how-to-populate-recyclerview-with-firebase-data-using-firebaseui-in-android-studio/
 
 public class ViewAnnouncementsFragment extends Fragment {
     private FragmentViewAnnouncementsBinding binding;
     FirebaseDatabase db;
+    private RecyclerView recyclerView;
+    private AnnouncementAdaptor adaptor;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -29,7 +37,22 @@ public class ViewAnnouncementsFragment extends Fragment {
         View root = binding.getRoot();
 
         // Link to realtime database
-        db = FirebaseDatabase.getInstance("https://cscb07project-c6a1c-default-rtdb.firebaseio.com/");
+        db = MainActivity.db;
+        // Recycler view
+        recyclerView = root.findViewById(R.id.recycler_announcements);
+        // Display the Recycler view linearly
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        // Class from FirebaseUI to make a query to the database to fetch data
+        FirebaseRecyclerOptions<Announcement> options
+                = new FirebaseRecyclerOptions.Builder<Announcement>()
+                .setQuery(db.getReference().child("announcements"), Announcement.class)
+                .build();
+        // Adaptor
+        adaptor = new AnnouncementAdaptor(options);
+        // Connect adaptor with Recycler view
+        recyclerView.setAdapter(adaptor);
+        // Start listening
+        adaptor.startListening();
 
         return root;
     }
@@ -37,6 +60,8 @@ public class ViewAnnouncementsFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        // Stop listening
+        adaptor.startListening();
         binding = null;
     }
 }
