2a6d94923c89a0c09573037751e98131e8e03c73
Vincent W <93243454+vincent-vw@users.noreply.github.com>
2023-11-27 00:02:50 -0500

    New text box

--- Application/app/src/main/java/com/example/cscb07project/MainActivity.java
+++ Application/app/src/main/java/com/example/cscb07project/MainActivity.java
@@ -51,7 +51,8 @@ public class MainActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_create_announcement,
-                R.id.nav_view_announcements, R.id.nav_sign_up, R.id.nav_login)
+                R.id.nav_view_announcements, R.id.nav_sign_up, R.id.nav_login,
+                R.id.nav_complaint_form, R.id.nav_feedback)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
