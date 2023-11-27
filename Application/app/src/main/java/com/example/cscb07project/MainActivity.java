ebfd1da0ed3d82aa7fa9a71ffa125bfa3047bbc7
libzee <theliabazeeshan@gmail.com>
2023-11-27 15:43:48 -0500

    POSt Requirements by Liaba

--- Application/app/src/main/java/com/example/cscb07project/MainActivity.java
+++ Application/app/src/main/java/com/example/cscb07project/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.cscb07project;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.TextView;
+import android.widget.Button;
 
-import com.google.android.material.snackbar.Snackbar;
+
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
@@ -15,10 +19,13 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cscb07project.databinding.ActivityMainBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
     FirebaseDatabase db;
@@ -31,19 +38,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+        //@Override
+        //public void onClick(View view) {
+        //    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        //            .setAction("Action", null).show();
+        //}
+        // });
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_create_announcement)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_create_announcement, R.id.nav_require)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
@@ -53,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
         // Link to realtime database
         db = FirebaseDatabase.getInstance("https://cscb07project-c6a1c-default-rtdb.firebaseio.com/");
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ Application/app/src/main/java/com/example/cscb07project/ui/requirement/Q2_comp.java
@@ -0,0 +1,42 @@
+package com.example.cscb07project.ui.requirement;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cscb07project.R;
+
+public class Q2_comp extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.questions2_comp);
+
+        Button buttonMin = findViewById(R.id.button_min);
+
+        buttonMin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Q3A_comp.class);
+                startActivity(intent);
+            }
+        });
+
+        Button buttonMaj = findViewById(R.id.button_maj_spec);
+
+        buttonMaj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Q3B_comp.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+}
