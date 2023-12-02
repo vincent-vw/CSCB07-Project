5a52f9242d61df0514741205a7a4bfdf2f36e026
Vincent W <93243454+vincent-vw@users.noreply.github.com>
2023-12-02 14:42:52 -0500

    Fixes

---
+++ Application/app/src/main/java/com/example/cscb07project/ui/requirements/Q3aStat.java
@@ -0,0 +1,21 @@
+package com.example.cscb07project.ui.requirements;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.cscb07project.R;
+public class Q3aStat extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.questions3a_stat, container, false);
+
+        return view;
+    }
+}
