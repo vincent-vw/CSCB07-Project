9584e6945a34601360a43f0e07fe487ce29d9fda
Hajime12341234 <132004336+Hajime12341234@users.noreply.github.com>
2023-12-02 02:49:21 -0500

    minor changes

--- Application/app/src/test/java/com/example/cscb07project/ExampleUnitTest.java
+++ Application/app/src/test/java/com/example/cscb07project/ExampleUnitTest.java
@@ -1,25 +1,22 @@
 package com.example.cscb07project;
 
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
-import androidx.navigation.NavController;
 
 import com.example.cscb07project.ui.User;
 import com.example.cscb07project.ui.account.LoginFragmentModel;
 import com.example.cscb07project.ui.account.LoginFragmentPresenter;
 import com.example.cscb07project.ui.account.LoginFragmentView;
 import com.google.firebase.database.DataSnapshot;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -28,6 +25,7 @@ import com.google.firebase.database.DataSnapshot;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest {
+
     @Mock
     private LoginFragmentView view;
 
@@ -42,12 +40,35 @@ public class ExampleUnitTest {
     @Before
     public void setUp () {
         presenter = new LoginFragmentPresenter(view, model);
-        snapshot = mock(DataSnapshot.class);
+        snapshot = Mockito.mock(DataSnapshot.class);
+
     }
 
+    // every field is unfilled
     @Test
     public void testEmptySignIn () {
         presenter.signIn("", "", -1);
+        verify(view).outputToast("Don't leave username, password, or selection blank.");
+    }
+
+    // username is the only unfilled thing
+    @Test
+    public void testSignInWithEmptyUsername() {
+        presenter.signIn("", "password", R.id.radioButton_student_login);
+        verify(view).outputToast("Don't leave username, password, or selection blank.");
+    }
+
+    // password is the only unfilled thing
+    @Test
+    public void testSignInWithEmptyPassword() {
+        presenter.signIn("username", "", R.id.radioButton_student_login);
+        verify(view).outputToast("Don't leave username, password, or selection blank.");
+    }
+
+    // empty radio button, but username and password are filled in
+    @Test
+    public void testSignInWithEmptyRadioButton() {
+        presenter.signIn("username", "password", -1);
         verify(view).outputToast("Don't leave username, password, or selection blank.");
     }
 
@@ -63,7 +84,6 @@ public class ExampleUnitTest {
         verify(model).queryDB(any(LoginFragmentPresenter.class), any(User.class));
     }
 
-
     @Test
     public void testNoSnapShotExists () {
         when(snapshot.exists()).thenReturn(false);
@@ -73,8 +93,9 @@ public class ExampleUnitTest {
         verify(view).navigate(R.id.action_nav_login_to_nav_sign_up);
     }
 
+    // testing signInFinalize with the correct student credentials
     @Test
-    public void testSnapShotExistsWithCorrectPass () {
+    public void testSignInFinalizeWithCorrectStudentCredentials () {
         when(snapshot.exists()).thenReturn(true);
         when(snapshot.getValue(User.class)).thenReturn(new User("test", "test123", "student"));
         User user = new User("test", "test123", "student");
@@ -85,12 +106,38 @@ public class ExampleUnitTest {
         verify(view).navigate(R.id.action_nav_login_to_nav_home);
     }
 
+    // testing signInFinalize with the wrong student credentials
     @Test
-    public void testSnapShotExistsWithWrongPass () {
+    public void testSignInFinalizeWithWrongStudentCredentials () {
         when(snapshot.exists()).thenReturn(true);
         when(snapshot.getValue(User.class)).thenReturn(new User("test", "test12", "student"));
         User user = new User("test", "test123", "student");
         presenter.signInFinalize(snapshot, user);
         verify(view).outputToast("Incorrect password. Please try again.");
     }
+
+    // testing signInFinalize with the correct admin credentials
+    @Test
+    public void testSignInFinalizeWithCorrectAdminCredentials() {
+        when(snapshot.exists()).thenReturn(true);
+        when(snapshot.getValue(User.class)).thenReturn(new User("adminUser", "adminPassword", "admin"));
+        User user = new User("adminUser", "adminPassword", "admin");
+        presenter.signInFinalize(snapshot, user);
+        assertEquals(MainActivity.user, user);
+        verify(view).signInSuccessful(user);
+        verify(view).outputToast("Successfully logged in! Welcome, " + user.getUsername() + "!");
+        verify(view).navigate(R.id.action_nav_login_to_nav_home);
+    }
+
+    // testing signInFinalize with the wrong admin credentials
+    @Test
+    public void testSignInFinalizeWithWrongAdminCredentials() {
+        when(snapshot.exists()).thenReturn(true);
+        when(snapshot.getValue(User.class)).thenReturn(new User("adminUser", "correctPassword", "admin"));
+        User user = new User("adminUser", "incorrectPassword", "admin");
+        presenter.signInFinalize(snapshot, user);
+        verify(view).outputToast("Incorrect password. Please try again.");
+    }
+
+
 }
